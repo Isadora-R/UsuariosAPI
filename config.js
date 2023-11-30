ccede36d7cd50b67920f568f1eb513866f962b40
Isadora-R <d2022015952@unifei.edu.br>
2023-11-30 01:29:47 -0300

    primeira versao da api
    v 1.0

---
+++ config.js
@@ -0,0 +1,10 @@
+﻿const dotenv = require('dotenv');
+dotenv.config();
+const {
+    PORT,
+    pgConnection
+} = process.env;
+module.exports = {
+    port: PORT,
+    urlConnection: pgConnection
+} 
